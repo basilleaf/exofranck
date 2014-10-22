8e9ef17f6760adcfe9d4d6a586bdaf0df46320d3
lisa ballard <lballard.cat@gmail.com>
2014-10-22 14:23:58 -0700

    improves feedback when link_checker finds broken link

--- link_checker.py
+++ link_checker.py
@@ -1,11 +1,19 @@
 import csv
 import urllib2
 
-with open('short_urls.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        for url in row:
-          f = urllib2.urlopen(urllib2.Request(url))
-          print "ok: %s" % url
+def check_all_links():
+    with open('short_urls.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            for url in row:
+                try:
+                    urllib2.urlopen(urllib2.Request(url))
+                    print "ok: %s" % url
+                except:
+                    print "not found --> %s " % url
+                    return
 
-print "All links ok, Bye!"
+    print "All links ok, Bye!"
+
+if __name__ == "__main__":
+    check_all_links()
