4855dff401f573de9cccacdab156413877b20f44
lisa ballard <lballard.cat@gmail.com>
2014-10-24 20:14:39 -0700

    removes hard-coded comment form in homepage template

--- wordpress/wp-content/themes/wpex-elegant-premium/templates/home.php
+++ wordpress/wp-content/themes/wpex-elegant-premium/templates/home.php
@@ -121,7 +121,7 @@ get_header(); ?>
 					<?php } ?>
 					<?php wp_reset_postdata(); ?>
 				</article><!-- #post -->
-				<?php comments_template(); ?>
+				<!-- ?php comments_template(); ? -->
 			<?php endwhile; ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
