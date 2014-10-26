f7430ac1ec74ba2ad88b7b265c79c7f8cd0e99ac
lisa ballard <lballard.cat@gmail.com>
2014-10-26 13:59:56 -0700

    creates a child theme that doesn't break things, updates alt/title tags in page images

--- wordpress/wp-content/themes/wpex-elegant-premium-child/functions.php
+++ wordpress/wp-content/themes/wpex-elegant-premium-child/functions.php
@@ -1,22 +1,19 @@
 <?php
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
 
-add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
-function enqueue_child_theme_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
-}
+/* Add custom functions below */
 
-
+# add bootstrap
+# breaks theme slider
+/*
 add_action('wp_head','hook_bootstrap_cdn');
-
 function hook_bootstrap_cdn()
 {
-
-$output='<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">';
-
-echo $output;
+    $output='<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">';
+    echo $output;
 }
-
+*/
 
 
 ?>
