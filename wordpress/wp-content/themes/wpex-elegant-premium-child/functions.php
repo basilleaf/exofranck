8dc468aee876812e709f4f521cc5e3036c9295b8
lisa ballard <lballard.cat@gmail.com>
2014-10-25 20:32:06 -0700

    adds bootstrap via cdn, adds css to prevent default wonky mouse interaction on wikisky iframe, adjusts post layout

--- wordpress/wp-content/themes/wpex-elegant-premium-child/functions.php
+++ wordpress/wp-content/themes/wpex-elegant-premium-child/functions.php
@@ -6,4 +6,17 @@ function enqueue_child_theme_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
 }
 
+
+add_action('wp_head','hook_bootstrap_cdn');
+
+function hook_bootstrap_cdn()
+{
+
+$output='<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">';
+
+echo $output;
+}
+
+
+
 ?>
