ab4021ec5ee1406749ea32b421f94b80d98e1686
lisa ballard <lballard.cat@gmail.com>
2014-11-01 00:26:55 -0700

    adds way to get images from wikisky locally and post them to wp, adds way to get and display list of planets for each star system

--- wp_post.py
+++ wp_post.py
@@ -1,6 +1,9 @@
+import os.path
 import csv
 import requests
 import jinja2
+import collections
+from make_bitly_links import all_slugs
 from json import dumps, loads
 from time import sleep
 from random import randint
@@ -9,11 +12,15 @@ from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import NewPost, GetPosts, EditPost
 from wordpress_xmlrpc.compat import xmlrpc_client
 from wordpress_xmlrpc.methods import media
+import urllib, cStringIO
 
 debug = False
 
 wp = Client(WP_XMLRPC_URL, WP_USER, WP_PW)
 all_posts = wp.call(GetPosts({'number':500}))
+
+# column labels for the iau csv
+iau_labels = ['star','lookUP_name','planet','Jup_mass','Earth_mass','Period_day','semi_ajor_axis_au','discovered_year','Constellation_en','Visibility','V_magnitude']
 
 def get_wp_post_id(slug):
 
@@ -63,6 +70,72 @@ def add_or_edit_wp_post(title, content, slug, more_info_url, local_img_file):
     print "%s %s as %s" % (msg, title, post.slug)
 
 
+def get_planet_status(star_slug):
+    """ returns the 3rd column of short_urls.csv for this star, which
+        indicates how many planets need names and whether star needs name too
+        """
+    with open('short_urls.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            star = row[1].split('/').pop()
+            if star_slug == star:
+                return row[2].strip()
+
+def fetch_all_images_from_lookUP():
+    """ this missed the list below (twice) but they do exist
+    """
+    # all_slugs = all_slugs()
+    all_slugs = ['bd-10-3166',
+                'bd-17-63',
+                'corot-2',
+                'corot-4',
+                'moa-2007-blg-192-l',
+                'moa-2007-blg-400-l',
+                'ogle-05-071l',
+                'ogle-05-169l',
+                'ogle-05-390l',
+                'ogle235-moa53',
+                'ogle2-tr-l9',
+                'ogle-tr-10',
+                'ogle-tr-111',
+                'ogle-tr-113',
+                'ogle-tr-132',
+                'ogle-tr-182',
+                'ogle-tr-211',
+                'ogle-tr-56',
+                'wasp-11-hat-p-10',
+                'ogle-06-109l',
+                'ogle-06-109l',
+                'psr-1257-12',
+                'psr-1257-12',
+                'psr-1257-12']
+    for slug in all_slugs:
+
+        # fetch the lookUP json feed
+        lookUP_name = slug.replace('-','+')
+        url = "http://www.strudel.org.uk/lookUP/json/?name=%s" % lookUP_name
+        lookUP_json = loads(requests.get(url).text)
+
+        # wikisky image
+        try:
+            img = lookUP_json['image']['src']
+            file = cStringIO.StringIO(urllib.urlopen(img).read())
+            img = Image.open(file)
+            img.save('images/%s.jpeg' % slug)
+
+        except KeyError:
+            print "no image for %s" % slug
+
+def get_planets(slug):
+    planets = []
+    for this_planet in get_iau_list():
+        if slug == this_planet['star'].strip().lower().replace(' ','-'):
+            planets.append(this_planet)
+
+    return planets
+
+
+
 def create_the_content(this_planet):
 
     templateLoader = jinja2.FileSystemLoader( searchpath="/" )
@@ -76,6 +149,18 @@ def create_the_content(this_planet):
     context['slug'] = context['star'].strip().lower().replace(' ','-')
     context['this_planet'] = this_planet
 
+    # planet count/star naming status
+    planet_status = get_planet_status(context['slug'])
+    star_needs_name = True
+    if planet_status == '1-sans-star':
+        planet_count = 1
+        star_needs_name = False
+    else:
+        planet_count = int(planet_status)
+
+    context['planet_count'] = planet_count
+    context['star_needs_name'] = star_needs_name
+
     # fetch the lookUP json feed
     lookUP_name = this_planet['lookUP_name'].replace(' ','+')
     url = "http://www.strudel.org.uk/lookUP/json/?name=%s" % lookUP_name
@@ -85,30 +170,37 @@ def create_the_content(this_planet):
     try:
         context['wikisky_link'] = lookUP_json['image']['href']
         context['wikisky_src'] = lookUP_json['image']['src']
+    except KeyError:
+        print "no image for %s" % this_planet
 
-        # virtualsky embed
+    # virtualsky embed
+    try:
         ra = lookUP_json['ra']['decimal']
         dec = lookUP_json['dec']['decimal']
         star = this_planet['lookUP_name'].replace(' ', '+')
         embed_url = "http://lcogt.net/virtualsky/embed/?projection=gnomic&ra=%s&dec=%s&showdate=false&showposition=false&constellationlabels=true&constellationboundaries=true&fov=50&objects=%s" % (ra, dec, star)
         context['virtualsky_url'] = embed_url
-
     except KeyError:
         print "no image for %s" % this_planet
 
+    context['simbad_link'] = "http://simbad.u-strasbg.fr/simbad/sim-basic?Ident=%s&submit=SIMBAD+search" % lookUP_name
+    context['simbad_link'] = "http://simbad.u-strasbg.fr/simbad/sim-basic?Ident=%s" % lookUP_name
+
+    context['planets'] = get_planets(context['slug'])
 
     content = template.render(context)
+
+    content = ' '.join([s.strip() for s in content.splitlines()]).strip()
 
     return context['slug'], context['title'], content
 
 
 def get_iau_list():
-    labels = ['star','lookUP_name','planet','Jup_mass','Earth_mass','Period_day','semi_ajor_axis_au','discovered_year','Constellation_en','Visibility','V_magnitude']
     with open('iau_list.csv') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
 
-            this_planet = dict(zip(labels, row))
+            this_planet = dict(zip(iau_labels, row))
             yield this_planet
 
 
@@ -125,7 +217,14 @@ def post_all_systems():
 
         posted.append(slug)
 
-        add_or_edit_wp_post(title, content, slug, '', '')
+        # check for an image
+        image_file = ''
+        img_path = "images/%s.jpeg" % slug
+        if os.path.isfile(img_path):
+            image_file = img_path
+
+        image_file = ''  # just editing posts at this point, no new ones
+        add_or_edit_wp_post(title, content, slug, '', image_file)
 
         print "http://exoplanets.seti.org/%s" % slug
 
